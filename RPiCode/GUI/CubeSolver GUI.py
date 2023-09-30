13135efc7bc67afbbffec9b5f4f169a28082b365
BuildTools <unconfigured@null.spigotmc.org>
2023-09-30 18:57:26 +0200

    gui test

--- RPiCode/GUI/CubeSolver GUI.py
+++ RPiCode/GUI/CubeSolver GUI.py
@@ -79,6 +79,7 @@ while True:
 
         if layout == 3:
             time.sleep(1)
+            ser.flush()
             while (ser.in_waiting == 0):
                 i=0
             ScanErgebnis = ser.readline()
